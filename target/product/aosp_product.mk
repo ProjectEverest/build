a5f3b44ccd1f25337699bd2d037089c1d8828c20
Michael Bestas <mkbestas@lineageos.org>
2024-03-13 20:09:52 +0200

    Remove WallpaperPicker from aosp_product.mk
    This package has been removed from AOSP in 14 QPR2
    
    Test: m
    Change-Id: I12c038d79967eece3a37bc3d2b19ffd979f54fee

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -30,8 +30,7 @@ PRODUCT_PACKAGES += \
     messaging \
     PhotoTable \
     preinstalled-packages-platform-aosp-product.xml \
-    ThemePicker \
-    WallpaperPicker \
+    ThemePicker
 
 # Telephony:
 #   Provide a APN configuration to GSI product
